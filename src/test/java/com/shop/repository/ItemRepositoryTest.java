2d2dedad255f4b62c37f09a019f0d42d4d471a3b
김종하 <81893082+gbr7780@users.noreply.github.com>
2022-07-12 11:22:34 +0900

    2장 p98 소스 업로드

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -26,7 +26,7 @@ class ItemRepositoryTest {
         for(int i=1;i<=10;i++){
         Item item = new Item();
         item.setItemNm("테스트 상품" + i);
-        item.setPrice(10000);
+        item.setPrice(10000 + i);
         item.setItemDetail("테스트 상품 상세 설명"+ i);
         item.setItemSellStatus(ItemSellStatus.SELL);
         item.setStockNumber(100);
@@ -44,6 +44,28 @@ class ItemRepositoryTest {
         List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
         for (Item item : itemList
              ) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for (Item item: itemList
+             ) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("nativeQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for (Item item: itemList
+        ) {
             System.out.println(item.toString());
         }
     }
