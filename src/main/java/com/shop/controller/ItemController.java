28e258c8c89a2773332d0ea5fb829071efd09721
김종하 <81893082+gbr7780@users.noreply.github.com>
2022-08-12 01:10:52 +0900

    4장 260p 소스코드

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -51,6 +53,22 @@ public class ItemController {
 
         // 상품이 정상적으로 등록되었다면 메인 페이지로 이동
         return "redirect:/";
+    }
+
+    @GetMapping(value = "/admin/item/{itemId}")
+    public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
+        try {
+            // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
+            ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+            model.addAttribute("itemFormDto", itemFormDto);
+        }
+        // 상품 엔티티가 존재하지 않을경우 에러메시지를 담아서 상품 등록 페이지로 이동한다.
+        catch (EntityNotFoundException e){
+            model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemForm";
+        }
+        return "item/itemForm";
     }
 
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -41,4 +44,24 @@ public class ItemService {
 
         return item.getId();
     }
+
+    // 상품 데이터를 읽어오는 트랜젹슨을 읽기 전용으로 설정한다. 성능 향상 및 아래에서 영속성 컨텍스트에 저장되는 내용이 없기에 읽기 전용으로 설정
+    @Transactional(readOnly = true)
+    public ItemFormDto getItemDtl(Long itemId){
+        // 메소드 매개 변수로 적은 상품의 이미지를 조회합니다. 등록순으로 가지고 오기 위해서 상품 이미지 아이디 오름차순으로 가져온다
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        // 조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어서 리스트에 추가
+        for (ItemImg itemImg: itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+        // 상품의 아이디를 통해 상품 엔티티를 조회한다.
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+        return itemFormDto;
+    }
+
+
 }
